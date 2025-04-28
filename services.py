5165a609443d44a873a719021c6a5280657817b1
Viktor242 <kamenevvv@yandex.ru>
2025-04-28 20:07:32 +0300

    Очистка репозитория от файлов IDE и исправление .gitignore

--- services.py
+++ services.py
@@ -1,19 +1,13 @@
-import tkinter as tk
-from tkinter import ttk
-
-
-
+# Функця добавления задачи
 def add_task(entry, task_listBox):
- 
-    
     task_text = entry.get()  # Получаем текст задачи из поля ввода
     if task_text:  # Проверяем, что поле не пустое
         # Добавляем задачу в Treeview с начальным статусом "Не начата"
         task_listBox.insert("", "end", values=(task_text, "Не начата"))  
         entry.delete(0, tk.END)  # Очищаем поле ввода после добавления
-
+        
+# Функця удаления задачи
 def delete_task(task_listBox):
-
     selected_item = task_listBox.selection()  # Получаем выбранный элемент
     if selected_item:  # Если элемент выбран
         task_listBox.delete(selected_item)  # Удаляем его
